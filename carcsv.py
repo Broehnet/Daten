5bb7b92f7c235deee22038f30d951a00086055e4
Broehnet <broehnet@gmail.com>
2022-06-03 08:22:36 +0200

    Hello

--- carcsv.py
+++ carcsv.py
@@ -35,6 +35,7 @@ def count(start=0, step=1):
 
 result = []
 counter = count()
+result2 = []
 with open('car_db_metric.csv', 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     csv_reader = list(reader(read_obj))[1:]
@@ -43,24 +44,26 @@ with open('car_db_metric.csv', 'r') as read_obj:
         #row variable is a list that represents a row in csv
         b, element = checkdata(row)
         if b:
-            result.append([next(counter)] + element)
+            result.append(element)
+    for e in result:
+        if ','.join(e) not in result2:
+            result2.append(','.join(e))
+    result = []
+    for e in result2:
+        result.append([next(counter)] + e.split(','))
 
-    with open('cardb.csv', mode='wb') as car_db_file:
+
+
+    with open('cardbe.csv', mode='wb') as car_db_file:
         cardb_writer = csv.writer(car_db_file, delimiter=',')
         cardb_writer.writerow(['make', 'model', 'trim', 'cylinder_layout', 'number_of_cylinders', 'engine_type', 'mixed_fuel_consumption_per_100km', 'city_fuel_per_100km', 'highway_fuel_per_100km'])
         for row in result:
             cardb_writer.writerow(row)
 
 
-with open('cardb.csv', 'r') as read_obj:
-    lst = list(reader(read_obj))
-    r = []
-    for e in lst:
-        if e[5] not in r:
-            r.append(e[5])
 
-    del r[0]
-    for e in r:
-        print(e)
+
+
+
 
 
